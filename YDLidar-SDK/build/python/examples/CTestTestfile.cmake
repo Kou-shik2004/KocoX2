c30572c7b046932aec04c1cc1e21502e62a086e5
Kou-shik2004 <skoushik20040804@gmail.com>
2024-11-03 01:18:45 +0530

    full push

---
+++ YDLidar-SDK/build/python/examples/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rpi/kocox2_ws/src/tortoisebot/YDLidar-SDK/python/examples
+# Build directory: /home/rpi/kocox2_ws/src/tortoisebot/YDLidar-SDK/build/python/examples
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
