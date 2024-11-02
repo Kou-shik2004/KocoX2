7b2c000a95693d7e1607066fd4b242da3205736c
Kou-shik2004 <skoushik20040804@gmail.com>
2024-11-03 01:09:16 +0530

    test run

---
+++ YDLidar-SDK/build/temp.linux-aarch64-3.10/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/rpi/kocox2_ws/src/tortoisebot/YDLidar-SDK
+# Build directory: /home/rpi/kocox2_ws/src/tortoisebot/YDLidar-SDK/build/temp.linux-aarch64-3.10
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("core")
+subdirs("src")
+subdirs("examples")
+subdirs("python")
